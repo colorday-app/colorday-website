cf9fa150817329851f7be41040fd794727a9300e
Di Wu <brycenycuu@gmail.com>
2020-06-01 23:30:33 -0700

    Add test method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpClientModule, HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
   title = 'colorday-website';
+
+  // demoUrl = 'http://localhost:8080/demo';
+  demoUrl = 'http://ec2-52-206-182-239.compute-1.amazonaws.com:8080/demo';
+
+  test() {
+    const test = this.http.request('GET', this.demoUrl, {headers: {'Access-Control-Allow-Origin': '*'}}).subscribe((data) => {
+      console.log(data);
+      const dataString = JSON.stringify(data);
+      alert(dataString);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
